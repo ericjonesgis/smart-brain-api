584e7045f602011abcbc41f8711645c92ad21dcd
Eric Jones <ericjones000000@gmail.com>
2020-07-08 20:57:06 -0400

    Adding port

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.get('/passwordreset', (req, res) => { passwordreset.handlePasswordReset(req,
 
 
 
-app.listen(3000, () => {
-	console.log('app is running on port 3000')
+app.listen(process.env.PORT || 3000, () => {
+	console.log('app is running on port ${process.env.PORT}')
 })
 
