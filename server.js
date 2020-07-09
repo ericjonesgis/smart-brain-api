6da3014e2a9ce7325a5237bc5a449404070eb51d
Eric Jones <ericjones000000@gmail.com>
2020-07-09 18:18:34 -0400

    Altered smtpTransport'

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ const passwordreset = require('./controllers/passwordreset');
     STMP is mail server which is responsible for sending and recieving email.
 */
 const smtpTransport = nodemailer.createTransport({
-    service: "Gmail",
     secure: false,
     port: process.env.PORT,
     auth: {
