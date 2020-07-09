ebcc733c3b0bf682f74799c1da8f5dac72a4d171
Eric Jones <ericjones000000@gmail.com>
2020-07-09 16:27:25 -0400

    changes

--- controllers/register.js
+++ controllers/register.js
@@ -46,8 +46,8 @@ const handleRegister = (req, res, db, bcrypt, smtpTransport) => {
 		    .then(trx.commit)
 			.catch(trx.rollback)
 		})
-		.catch(err => {
-			res.status(400).json('Unable to register. Email may already be registered. Ensure you have verified your e-mail before signing in.')
+		//.catch(err => {
+			//res.status(400).json('Unable to register. Email may already be registered. Ensure you have verified your e-mail before signing in.')
 		})
 
 	}
