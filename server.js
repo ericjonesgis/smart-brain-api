92a1641310a931edc951bbd4d5b6d30a8ce80284
Eric Jones <ericjones000000@gmail.com>
2020-07-09 12:14:37 -0400

    Changed database connection string to work with Heroku postgres database

--- server.js
+++ server.js
@@ -42,10 +42,8 @@ const smtpTransport = nodemailer.createTransport({
 const db = knex ({ 
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'postgres',
-    database : 'smart-brain'
+  	connectionstring: process.env.DATABASE_URL,
+  	ssl: true 
   }
 });
 
