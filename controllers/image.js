0b46c2339b750f77957bce353406dbca6122ee65
Eric Jones <ericjones000000@gmail.com>
2020-07-10 15:25:28 -0400

    changing handleImage function

--- controllers/image.js
+++ controllers/image.js
@@ -5,10 +5,25 @@ const app = new Clarifai.App({
 });
 
 const handleApiCall = (req, res, db) => {
-	const { input, email } = req.body;
-	console.log(input);
-	console.log(email);
-	db.transaction(trx => {
+	const { input } = req.body;
+	app.models
+		.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+		.then(data => {
+			res.json(data);
+		}).catch(err => res.status(400).json('unable to work with API'))
+
+}
+
+const handleImage = (req, res, db) => {
+	const { id, input, email } = req.body;
+	db('users').where('id', '=', id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0]);	
+	})
+	.then(insert => {
+		db.transaction(trx => {
 			trx.insert({
 				imageurl: input,
 				email: email
@@ -17,21 +32,6 @@ const handleApiCall = (req, res, db) => {
 		    .then(trx.commit)
 			.catch(trx.rollback)
 		})
-	app.models
-		.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-		.then(data => {
-			res.json(data);
-		}).catch(err => res.status(400).json('This URL is unable to work with API'))
-
-}
-
-const handleImage = (req, res, db) => {
-	const { id } = req.body;
-	db('users').where('id', '=', id)
-	.increment('entries', 1)
-	.returning('entries')
-	.then(entries => {
-		res.json(entries[0]);	
 	})
 	.catch(err => res.status(400).json('unable to get entries'));
 }
