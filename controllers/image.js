31f37b482c95da19ee6e044307005febe54985a9
Eric Jones <ericjones000000@gmail.com>
2020-07-10 11:26:03 -0400

    Adding extra messaging for image detection component

--- controllers/image.js
+++ controllers/image.js
@@ -21,7 +21,7 @@ const handleApiCall = (req, res, db) => {
 		.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
 		.then(data => {
 			res.json(data);
-		}).catch(err => res.status(400).json('unable to work with API'))
+		}).catch(err => res.status(400).json('This URL is unable to work with API'))
 
 }
 
